e1213e0cb5f7aa89568f5f325450f035bdaa6b24
michoski <michoski@gmail.com>
2019-01-06 18:25:25 -0600

    cem

--- q_training.py
+++ q_training.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import copy
 import pickle
 from q_learning import Game, QPlayer     # Classes used for Tic Tac Toe
+import shutil
 
 root = tk.Tk()
 epsilon = 0.9
@@ -10,7 +11,7 @@ player1 = QPlayer(mark="X",epsilon = epsilon)
 player2 = QPlayer(mark="O",epsilon = epsilon)
 game = Game(root, player1, player2)
 
-N_episodes = 10000
+N_episodes = 10
 N = 3
 for episodes in range(N_episodes):
     game.play()
@@ -19,4 +20,9 @@ for episodes in range(N_episodes):
 Q = game.Q
 
 filename = "Q_epsilon_09_Nepisodes_{}_N={}.p".format(N_episodes, N)
-pickle.dump(Q, open(p_files/filename, "wb"))
+pickle.dump(Q, open(filename, "wb"))
+
+source = '/home/michoski/ndgame/nd_tictactoe/'
+destination = '/home/michoski/ndgame/nd_tictactoe/p_files/'
+
+shutil.move(source+filename,destination)
